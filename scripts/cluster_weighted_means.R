9debc1c0e7d82f6b923e07913aa1674c87f096fd
samvancart <sam02cart@gmail.com>
2023-10-16 11:08:10 +0300

    cluster_aggregates.R, update basal area function

--- scripts/cluster_weighted_means.R
+++ scripts/cluster_weighted_means.R
@@ -1,4 +1,5 @@
 source('scripts/settings.R')
+source('./r/cluster_aggregates.R')
 
 # Get clusters before running
 
@@ -11,68 +12,29 @@ source('scripts/settings.R')
 # 5. Write cluster_weighted_means file
 
 
-# Calculate basal area: pi*(dbh/200)^2*"multiplier Ntrees in data"
-get_basal_area_nfi_sweden <- function(x) {
-  dbh <- as.double(x[6])
-  multiplier <- as.double(x[15])
-  basal_area <- pi*(dbh/200)^2*multiplier
-  return(basal_area)
-}
+
+# Basal area for NFI Sweden
+path <- paste0("data/nfi/sweden/all_sorted_group_species_cIDs_speciesID11to4.csv")
+df <- fread(path)
+df$Dbh <- df$Dbh/10
 
 
+# Apply to each df row to get basal_area vector
+basal_area <- apply(df, 1, get_basal_area, dbh_col=6, multiplier_col=15)
 
-# # Basal area for NFI Sweden
-# path <- paste0("C:/Users/samu/Documents/yucatrote/r/forest_navigator23_r/data/nfi/sweden/all_sorted_group_species_cIDs.csv")
-# df <- fread(path)
-# df$Dbh <- df$Dbh/10
-# 
-#  
-# # Apply to each df row to get basal_area vector
-# basal_area <- apply(df, 1, get_basal_area_nfi_sweden)
-# 
-# 
-# # Add vector as column to df
-# df$basal_area <- basal_area
-# 
-# path <- paste0("C:/Users/samu/Documents/yucatrote/r/forest_navigator23_r/data/nfi/sweden/sorted_group_species_cIDs_basal_area.csv")
-# write.csv(df, path, row.names = F)
+# Add vector as column to df
+df$basal_area <- basal_area
 
-# Aggregate sums of variable based on groupID, speciesID, clusterID
-get_variable_sums <- function(df, var) {
-  formula <- as.formula(paste0(var,"~groupID + speciesID + clusterID"))
-  df <- aggregate(formula, data=df, FUN=sum)
-  df <- rename_col(df, var, "_layer")
-  
-  return(df)
-}
-
-# Weighted aggregate means of a variable by groupID, speciesID, clusterID
-get_weighted_variable <- function(df,var,weighted_by="basal_area") {
-  
-  df[,var] <- df[, var]*df[,weighted_by]
-  weighted_layer <- paste0(weighted_by,"_layer")
-  df[,var] <- df[,var]/df[,weighted_layer]
-  formula <- as.formula(paste0(var,"~groupID + speciesID + clusterID"))
-  df <- aggregate(formula,FUN=sum,data=df)
-  
-  return(df)
-}
-
-
-rename_col <- function(df,var,suffix){
-  new_name <- paste0(var,suffix)
-  df <- df %>% rename(!!new_name := var)
-  return(df)
-}
+path <- paste0("data/nfi/sweden/sorted_group_species_cIDs_speciesID11to4_basal_area.csv")
+write.csv(df, path, row.names = F)
 
 
 # # # NFI DATA
-path <- paste0("C:/Users/samu/Documents/yucatrote/r/forest_navigator23_r/data/nfi/sweden/sorted_group_species_cIDs_basal_area.csv")
+path <- paste0("data/nfi/sweden/sorted_group_species_cIDs_speciesID11to4_basal_area.csv")
 df <- fread(path)
 
-# data.table becomes tbl_df
-df <- df %>%
-  group_by(groupID)
+# Df to tibble
+df <- as_tibble(df)
 
 # 1. Get aggregate sums for basal_areas and Multiplier_tree_Number_Ha
 df_baLayers <- get_variable_sums(df, "basal_area")
@@ -100,8 +62,8 @@ dbh_qm <- unique(df_merged[c("groupID","speciesID","clusterID","dbh_qm_layer")])
 # 4. Merge dfs
 df_weighted <- merge(merge(merge(merge(dbh,h),age),df_baLayers_sorted),dbh_qm)
 
-# Plot
-plot(df_merged$Dbh,df_merged$dbh_qm_layer)
+# # Plot
+# plot(df_merged$Dbh,df_merged$dbh_qm_layer)
 
 # Rename col
 df_weighted <- df_weighted %>% rename("basal_area" = "basal_area_layer")
@@ -111,8 +73,8 @@ df_weighted_sorted <- df_weighted[with(df_weighted,order(groupID,speciesID,clust
 df <- df_weighted_sorted
 
 # Write csv
-# path <- paste0("C:/Users/samu/Documents/yucatrote/r/forest_navigator23_r/data/nfi/sweden/cluster_weighted_means.csv")
-# write.csv(df, path, row.names = F)
+path <- paste0("data/nfi/sweden/cluster_weighted_means_speciesID11to4.csv")
+write.csv(df, path, row.names = F)
 
 
 
